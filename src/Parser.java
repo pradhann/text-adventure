decac4625192d8d797cac8c94e11615762928887
Ashlin <Ashlin@132.161.245.62>
2017-05-10 20:33:37 -0500

    added to parser

--- src/Parser.java
+++ src/Parser.java
@@ -15,7 +15,32 @@ public class Parser {
 	}
 	
 	public void getCommand() {
-		
+		switch(words[0]) {
+		case "go":
+			
+			break;
+		case "use":
+			
+			break;
+		case "talk":
+			
+			break;
+		case "take":
+			
+			break;
+		case "wait":
+			
+			break;
+		case "attack":
+			
+			break;
+		case "look":
+			
+			break;
+		default:
+			System.out.println("I don't recognize that command.");
+			break;
+		}
 	}
 	
 
